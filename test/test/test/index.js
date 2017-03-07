99cb41e422d8b26e1b9d1d1e8fce4355732eb24e
Toshimitsu Sohara <toshimitsu@cansell.jp>
2017-03-07 15:39:18 +0900

    mod

---
+++ test/test/test/index.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const index = require('./../../../');
+
+describe('test', () => {
+  it('test', () => {
+    assert.equal(index(2), 4);
+//    assert.equal(index(2), 5);
+  });
+});
+
