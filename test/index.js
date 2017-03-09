c58386a2be85bbf5642502767e3ead0c98cbc742
Toshimitsu Sohara <toshimitsu@cansell.jp>
2017-03-09 11:31:31 +0900

    env

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 const assert = require('assert');
 const index = require('./../');
+const express = require('express');
 
 describe('test', () => {
   it('test', () => {
+    var app = express();
+    console.log("env: " + app.get('env'));
     assert.equal(index(2), 4);
 //    assert.equal(index(2), 5);
   });
